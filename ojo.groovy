f007d82f00366722fcf90b05388c3d9ee3fcea9f
Itamar Berman-Eshel <Itamar@Itamars-MacBook-Pro.local>
2015-12-30 15:23:31 +0200

    added json output to console

--- ojo.groovy
+++ ojo.groovy
@@ -16,12 +16,12 @@ swing.edt {
 	//def password = "your password (if needed)"
 	def baseURL = "http://oss.jfrog.org/artifactory/api/storage/oss-snapshot-local/"
 
-  frame(title: 'OJO path checker', size: [500, 200], show: true) {
+  frame(title: 'OJO path checker', size: [800, 200], show: true) {
     borderLayout()
     def input = textField(columns:10, actionPerformed: {}, constraints:BL.NORTH)
     def output = label(text: '', preferredSize: [100, 100], constraints: BL.SOUTH)
-	  def checkPath = button(text:'Check Path', actionPerformed: {println "curl ${baseURL}${input.text}".execute().text; output.text = "<html>Searching OJO for path: ${input.text}<br>Please see console output for results<html>"}, constraints:BL.WEST)
-    def checkProps = button(text:'Check properties', actionPerformed: {println "curl ${baseURL}${input.text}?properties".execute().text; output.text="<html>Searching OJO for properties on path: ${input.text}<br>Please see console output for results<html>"}, constraints:BL.EAST)
+	  def checkPath = button(text:'Check Path', actionPerformed: {def path = "curl ${baseURL}${input.text}".execute().text; println path; output.text = "<html>Searching OJO for path: ${input.text}<br><br>${path}<html>"}, constraints:BL.WEST)
+    def checkProps = button(text:'Check properties', actionPerformed: {def props = "curl ${baseURL}${input.text}?properties".execute().text; println props; output.text="<html>Searching OJO for properties on path: ${input.text}<br><br>${props}<html>"}, constraints:BL.EAST)
     widget(panel())
   }
 }
